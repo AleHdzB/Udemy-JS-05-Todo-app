1bd772d24cfbcbd89cb3b460d17bf211bc677628
AleHdzB <alejandro.hernandez4819@alumnos.udg.mx>
2026-02-24 22:47:14 -0600

    Fix blank page by delaying App init until DOM is ready

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './style.css';
 import {App} from './todos/app';
-import todoStore from './store/todo.store'
+import todoStore from './store/todo.store';
 
 // ensure application starts after DOM is ready (build injects script in <head>)
 todoStore.initStore();
